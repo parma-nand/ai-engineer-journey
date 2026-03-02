8a8d5a6df971b7ac3c61f2c1122814f1d4fc1d96
parma-nand <real.me.parma@gmail.com>
2026-03-02 09:37:44 +0530

    Day 3 Pandas

--- day3.py
+++ day3.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
 df = pd.read_csv(url)
+print(df.info())
+print(df.describe())
 # print("Shape:\n", df.shape)
 # print("Columns\n",df.columns.to_list())
 # print("Datatype \n",df.dtypes)
@@ -16,4 +18,4 @@ df = pd.read_csv(url)
 # print("Missing Age \n",df['Age'].isna().sum())
 # df['Age']=df['Age'].fillna(df['Age'].mean())
 # print("Missing Age Now\n",df['Age'].isna().sum())
-print("Group By Pclass and Mean of Survived\n",df.groupby('Pclass')['Survived'].mean()*100)
+# print("Group By Pclass and Mean of Survived\n",df.groupby('Pclass')['Survived'].mean()*100)
