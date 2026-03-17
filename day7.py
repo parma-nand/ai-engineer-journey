43b0bc2de573dc4eea12ce3008ae93c1e158300e
parma-nand <real.me.parma@gmail.com>
2026-03-17 09:48:26 +0530

    Day 7 Revision and Mini Project

---
+++ day7.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+url="https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+df=pd.read_csv(url)
+
+class TitanicAnalyzer:
+    def __init__(self,df):
+        self.df=df
+
+    def clean(self):
+        # self.df['Age'].fillna(self.df['Age'].mean(), inplace=True) it is also correct but not recomended
+        self.df['Age']=self.df['Age'].fillna(self.df['Age'].mean())
+        self.df['Embarked']=self.df['Embarked'].fillna(self.df['Embarked'].mode()[0])
+        self.df=self.df.drop(columns=['Cabin','Name','Ticket'])
+        return self.df
+    def analyze(self):
+        print("="*35)
+        print("Titaninc Data Analysis")
+        print("="*35)
+        print(f"Total Passenger {len(self.df)}")
+        print(f"Total Passenger Survived {self.df['Survived'].sum()}")
+        print(f"Survival Rate {self.df['Survived'].mean()*100:.1f}%")
+        print(f" \n By Gender {self.df.groupby('Pclass')['Survived'].mean()}")
+        print(f" \n By Gender {self.df.groupby('Sex')['Survived'].mean()}")
+
+
+
+
+
+
+
+
+analyzer=TitanicAnalyzer(df)
+analyzer.clean()
+analyzer.analyze()
